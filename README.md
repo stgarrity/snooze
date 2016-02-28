6b75ab53209320d74dac6358c242e07e49dfad83
Steve Garrity <sgarrity@gmail.com>
2016-02-28 07:31:26 +0000

    initial commit of snooze

---
+++ README.md
@@ -0,0 +1,32 @@
+I've been using Mailbox (http://www.mailboxapp.com) since it was distributed via TestFlight and my email load broke the servers more than once ... and it quickly became an absolutely integratl part of my daily life, workflow, *tear* my family even *tear*. So obviously I was super sad to see it shut down last week. Rather than attempt to write a thoughtful, sentimental, or funny tribute (none of which I'm good at), I chose to memorialize my love for Mailbox in code.
+
+Aghast at having to live without snooze going forward (and yes, I played with all the other imitation clients, they just can't hold a candle to Mailbox), I hacked this together on a cab ride from Manhattan to Newark Airport. Enough people have asked me about it that I figured I'd share. It's still a work in progress (PRs welcome).
+
+This in no way holds a candle to Mailbox. The UX is whatever second-rate email client you're using now (you just move the message to one of four labels, "Tonight", "Tomorrow", "This Weekend", "Next Week" (all nested under ".Snooze", and auto-created for you on first run), and it will pull it back into your inbox at the appropriate time), it doesn't allow custom scheduling, swipes don't work as well, there's no auto-swiping, etc. But at least it gives you snooze back!
+
+All the credit here goes to the incredible team at Mailbox for changing the way so many of us looked at email, whether it was swipes, or unified inboxes, or snooze, or that Violet Hour of the Inbox Zero image of the day. Thanks for all that you did, guys & gals!
+
+Setup:
+
+1) copy email_snooze.py to a directory where you want it to live (preferrably on a server somewhere, we rely on cron to run this every day)
+
+2) edit the users = line (~5th to last line in the file) to have your email addresses, or at least the right number of emails you'd like to use with this (the actual strings are all relative to issued OAuth tokens in the app, so only used for debugging)
+
+3) create a Google API app using the wizard here: https://console.developers.google.com/start/api?id=gmail
+
+4) create client credentials for that app of type "Other" and download the JSON into "client_secret.json" in the same directory as email_snooze.py
+
+5) run "python email_snooze.py none --noauth_local_webserver" and follow the prompts to grant access to your email account(s)
+
+6) copy the lines from "crontab_file" into your crontab (using "crontab -e"), and making sure to insert the correct path to email_snooze.py (and potentially update your times)
+
+Known issues:
+
+- scheduling via cron is ghetto
+- current scheduling doesn't handle DST well if your computer timezone is UTC
+- very little error handling, and it won't tell you if it fails
+- does not currently remove the snooze label, so if you just archive the message it will effectively snooze again. This is "by design" but I'm far from convinced it's the right design. If you'd like to change it, simply add 
+
+"removeLabelIds": [search_label]
+
+to the body dict of the thread .modify() call per https://developers.google.com/gmail/api/v1/reference/users/threads/modify
---
+++ email_snooze.py
@@ -0,0 +1,125 @@
+# with credit to Google's Gmail API quickstart for Python: https://developers.google.com/gmail/api/quickstart/python
+
+from __future__ import print_function
+import httplib2
+import os
+import sys
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/[username].json
+SCOPES = 'https://www.googleapis.com/auth/gmail.modify'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Mailbox tribute label snoozer'
+
+LABEL_MAP = {'tonight': '.Snooze/1 - This Evening',
+             'tomorrow': '.Snooze/2 - Tomorrow',
+             'weekend': '.Snooze/3 - This Weekend',
+             'nextweek': '.Snooze/4 - Next Week'}
+
+try:
+    import argparse
+    parser = argparse.ArgumentParser(parents=[tools.argparser])
+    parser.add_argument('schedule', choices=LABEL_MAP.keys() + ["none"])
+    flags = parser.parse_args()
+except ImportError:
+    flags = None
+
+
+def get_credentials(username):
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+    Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   '%s.json' % username)
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def create_label(service, label_name):
+    service.users().labels().create(userId='me',
+                                  body={'name': label_name}).execute()
+
+def create_labels(service):
+    create_label(service, '.Snooze')  # need to create the top-level label first
+
+    for label_name in LABEL_MAP.values():
+        create_label(service, label_name)
+
+
+def find_label_id(labels, label_name):
+    for label in labels:
+        if label['name'] == label_name:
+            return label['id']
+    return None
+
+
+def run_snooze(service):
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    search_label_name = LABEL_MAP.get(flags.schedule)
+    if not search_label_name:
+        print("invalid argument")
+        sys.exit(2)  # FIXME should this validation be here?
+
+    inbox_label = find_label_id(labels, "INBOX")
+    search_label = find_label_id(labels, search_label_name)
+
+    # make sure the labels exist
+    if not inbox_label:
+        raise Exception("failed to find inbox...that's not good'")
+    if not search_label:
+        create_labels(service)
+        labels = service.users().labels().list(userId='me').execute().get('labels', [])  # FIXME factoring?
+        search_label = find_label_id(labels, search_label_name)
+        if not search_label:
+            raise Exception("failed to create labels")
+
+    # FIXME: doesn't support pagination
+    results = service.users().threads().list(userId="me", labelIds=search_label).execute()
+    threads = results.get('threads', [])
+
+    for thread in threads:
+        service.users().threads().modify(userId="me",
+                                         id=thread['id'],
+                                         body={'addLabelIds': [inbox_label]}).execute()
+
+
+def snooze_for_user(username):
+    credentials = get_credentials(username)
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    if flags.schedule != "none":
+        run_snooze(service)
+
+
+if __name__ == '__main__':
+    users = ['work@example.com', 'personal@example.com']
+
+    for user in users:
+        # FIXME error handling to alert me?
+        snooze_for_user(user)
